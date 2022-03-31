3a408857d4554e9e5cef951eb816ab7d07888af8
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2022-03-31 15:52:33 +0100

    BAU: Check for broken internal links
    Sometimes we break links (full or partial) when we move pages about or rename headers, so now html-proofer will run as part of the build to make sure we've not broken any links.

--- config.rb
+++ config.rb
@@ -1,5 +1,24 @@
 require 'govuk_tech_docs'
 
+# Check for broken links
+require 'html-proofer'
+
 GovukTechDocs.configure(self)
 
 set :layout, 'custom'
+
+after_build do |builder|
+  begin
+    HTMLProofer.check_directory(config[:build_dir],
+      { :assume_extension => true,
+        :disable_external => true,
+        :allow_hash_href => true,
+        :empty_alt_ignore => true,
+        :file_ignore => [
+            /search/ # Provided by tech-docs gem but has a "broken" link from html-proofer's point of view
+        ]
+      }).run
+  rescue RuntimeError => e
+    abort e.to_s
+  end
+end
