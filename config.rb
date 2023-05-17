9e2219efb6d30f02142bf908b288fff2c0c8bcbc
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2023-05-17 09:54:40 +0100

    BAU: Check internal hashes
    Also remove duplication between a single GitHub URL and the blanket GitHub regex

--- config.rb
+++ config.rb
@@ -12,12 +12,12 @@ after_build do |builder|
     proofer = HTMLProofer.check_directory(config[:build_dir],
       { :assume_extension => true,
         :allow_hash_href => true,
+        :check_internal_hash => true,
         :empty_alt_ignore => true,
         :file_ignore => [
             /search/ # Provided by tech-docs gem but has a "broken" link from html-proofer's point of view
         ],
         :url_ignore => [
-            "https://github.com/alphagov/centralised-security-logging-service",
             "https://gdshelpdesk.digital.cabinet-office.gov.uk",
             "https://gds-way.cloudapps.digital/standards/secrets-acl.html",
             /https:\/\/github.com\//
