5d39782ebdad42174c69d26cf30d31a7d61e678e
James Robinson <james.robinson@digital.cabinet-office.gov.uk>
2023-11-02 11:48:16 +0000

    Make HTML Proofer ignore links to new files
    n.b. `delete_suffix` is a no-op if the string does not end with that suffix

--- config.rb
+++ config.rb
@@ -9,6 +9,15 @@ set :layout, 'custom'
 
 after_build do |builder|
   begin
+    # Newly created files on feature branches do not yet exist in production, so tell HTML Proofer to ignore links to them
+    # Based on https://github.com/gjtorikian/html-proofer#ignoring-new-files
+    merge_base = `git merge-base origin/main HEAD`
+    new_urls = %x(git diff -z --name-only --diff-filter=AC #{merge_base})
+      .split("\0")
+      .filter { |path| path.start_with? "source/" }
+      .map { |path| path.delete_prefix("source/").delete_suffix(".erb").delete_suffix(".md") }
+      .map { |path| "https://gds-way.cloudapps.digital/#{path}" }
+
     proofer = HTMLProofer.check_directory(config[:build_dir],
       { :assume_extension => true,
         :allow_hash_href => true,
@@ -21,7 +30,7 @@ after_build do |builder|
             "https://gdshelpdesk.digital.cabinet-office.gov.uk",
             "https://gds-way.cloudapps.digital/standards/secrets-acl.html",
             /https:\/\/github.com\//
-        ]
+        ].concat(new_urls)
       })
 
       proofer.run
