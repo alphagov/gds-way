85ff7116c29b4a33a3988b647fa2f6c9a392578f
George Lund <george.lund@digital.cabinet-office.gov.uk>
2025-08-01 11:23:48 +0100

    Make docker vs podman a command line option

--- startup.sh
+++ startup.sh
@@ -2,17 +2,29 @@
 
 set -e
 
-CONTAINER_ENGINE=podman
+if [ "$1" = "--help" ]; then
+    echo "$0 [docker|podman]"
+    exit 1
+fi
 
-if [[ "$CONTAINER_ENGINE" == "podman" ]] && command -v podman >/dev/null 2>&1;
+ENGINE="$1"
+if [ "$ENGINE" = "" ] ; then
+    if command -v podman >/dev/null 2>&1; then
+        echo "Podman detected, launching in podman."
+        ENGINE="podman"
+    elif command -v docker >/dev/null 2>&1; then
+        echo "Docker detected, launching in docker."
+        ENGINE="docker"
+    fi
+fi
+
+if [ "$ENGINE" = "podman" ]
 then
-    echo "Podman detected, launching in podman."
     podman build . --tag gds-way
     # We need to force middleman into polling mode due to this podman issue: https://github.com/containers/podman/discussions/19430
     podman run --rm -p 4567:4567 -p 35729:35729 -v $(pwd):/usr/src/docs -it gds-way bash -c "bundle exec --gemfile=/usr/src/gems/Gemfile middleman --watcher-force-polling server"
-elif [[ "$CONTAINER_ENGINE" == "docker" ]] && command -v docker >/dev/null 2>&1;
+elif [ "$ENGINE" = "docker" ]
 then
-    echo "Docker detected, launching in docker."
     docker build . --tag gds-way
     docker run --rm -p 4567:4567 -p 35729:35729 -v $(pwd):/usr/src/docs -it gds-way
 else
