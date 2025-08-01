0429bb392ca02fac1d346d8e02a5a4f8040e7f16
Lev Gorbunov <lev.gorbunov@digital.cabinet-office.gov.uk>
2025-08-01 10:28:59 +0100

    ENG-896: Add comment explaining patch

--- patches/patch_listen.rb
+++ patches/patch_listen.rb
@@ -4,7 +4,7 @@ module Listen
   module Adapter
     class << self
       def select(_options = {})
-        Listen.logger.debug 'Monkey patch: forcing polling adapter'
+        # Force listener into polling mode, as inotifywait watcher is broken in podman: https://github.com/containers/podman/discussions/19430
         Adapter::Polling
       end
     end
